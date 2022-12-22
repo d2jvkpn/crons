fce9924ed116d3615b6aa24acfdd325cfb2c4175
d2jvkpn <chenbin2018@protonmail.com>
2022-12-22 09:27:48 +0800

    ...

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,7 +4,7 @@ _wd=$(pwd)
 _path=$(dirname $0 | xargs -i readlink -f {})
 
 bash scripts/go_build.sh
-tag=$(./target/crons -h 2>&1 | awk '/GitCommit/{print $2}' | cut -c -12)
+tag=$(./target/crons -h 2>&1 | awk '/GitCommit/{print $2; exit}' | cut -c -12)
 # d=crons_$(date +%F)_${tag}
 d=$(printf "crons_%s_%s" $(date +%F) $tag)
 mkdir -p target/${d}
