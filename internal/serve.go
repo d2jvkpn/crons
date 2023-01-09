e684b17a954813e60d3f5cd05f05c73f27d4469b
d2jvkpn <chenbin2018@protonmail.com>
2023-01-09 22:23:14 +0800

    ...

--- internal/serve.go
+++ internal/serve.go
@@ -10,7 +10,7 @@ import (
 )
 
 func Serve(addr string, meta map[string]any) (err error) {
-	Logger.Info("Server is starting", zap.String("address", addr), zap.Any("meta", meta))
+	Logger.Info("Server is starting", zap.Any("meta", meta))
 	_Server.Addr = addr
 
 	if err = _Server.ListenAndServe(); err != http.ErrServerClosed {
